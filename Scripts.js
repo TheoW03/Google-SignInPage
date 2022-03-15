d197b1fd92785ef130978f2de6145e5ae0391579
Theo <95878514+TheoW03@users.noreply.github.com>
2022-03-15 17:45:51 -0400

    Added fieldset. and trying to add animation

--- Scripts.js
+++ Scripts.js
@@ -2,13 +2,17 @@ var click = 0;
 function click2() {
     var element = document.getElementById("sign");
     var ignoreClickOnMeElement = document.getElementById('inpputUser');
+    var InpuEle = document.getElementById('inpputUser')
     document.addEventListener('click', function (event) {
         var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
         if (!isClickInsideElement) {
             element.innerHTML = '<legend></legend>';
+            InpuEle.placeholder = 'Email Or Phone';
         } else {
-            element.innerHTML = '<legend>email or phone</legend>';
+            element.innerHTML = '<legend>Email Or Phone</legend>';
+            InpuEle.placeholder = '';
         }
+        
     });
 
 }
